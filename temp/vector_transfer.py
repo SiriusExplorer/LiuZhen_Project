220a2a4c8d6cba4586bd1c3bec07ceb047ec0724
Liu Zhen <32029530+SiriusExplorer@users.noreply.github.com>
2018-01-10 22:39:35 +0100

    Delete vector_transfer.py

--- temp/vector_transfer.py
+++
@@ -1,7 +0,0 @@
-# The script transfer the vector_np_lam12 into vectors with different lambda value
-
-import numpy as np
-
-def vector_transfer(reset_reset_psepssm_lam = 1, reset_pseaac_lam = 1)
-
-vector_np_lam12 = np.load('')
