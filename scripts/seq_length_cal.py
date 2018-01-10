8c163b35f9566feab940bd85a08d30496b0ebf4a
Liu Zhen <32029530+SiriusExplorer@users.noreply.github.com>
2018-01-10 22:44:28 +0100

    Update and rename deeploc_dataset/2.seq_length_cal.py to scripts/seq_length_cal.py

--- scripts/seq_length_cal.py
+++ scripts/seq_length_cal.py
@@ -5,7 +5,7 @@ file = open('deeploc_testset', 'r')
 file_lines = file.readlines()
 seq_len = []
 for i in [odds for odds in range(len(file_lines)) if odds%2 != 0]:
-    seq_len = seq_len + [len(file_lines[i])]
+    seq_len = seq_len + [len(file_lines[i]) - 1]
 print('The minimum of the protein sequences length is:', min(seq_len))
 print('The maximum of the protein sequences length is:', max(seq_len))
 print('The average of the protein sequences length is: %.2f' % (sum(seq_len)/len(seq_len)))
